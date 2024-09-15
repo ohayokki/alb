9d8b7d0003cf545da9c3374db4aa24598e4707da
obara811 <ohayokki.mentor@gmail.com>
2024-09-15 11:58:31 +0900

    バグ修正

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,9 +1,8 @@
 class PrefecturesController < ApplicationController
   def index
-    #@prefecture = Prefecture.includes(districts: :areas).find_by(name: params[:id])
     @prefecture = Prefecture.includes(:districts).find_by(name: params[:id])
-    @genres = Genre.all
     @shops = @prefecture.shops.where(status: ["無料掲載", "有料掲載", "お試し有料掲載"]).includes(:area, :genre)
+    @genres = Genre.joins(:shops).where(shops: { id: @shops.select(:id) }).distinct
 
     if params[:latitude].present? && params[:longitude].present?
       @latitude = params[:latitude].to_f
