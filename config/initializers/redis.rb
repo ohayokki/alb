fe860dc5d9b414862bdaff0f8856dcaf6a4466fa
obara811 <ohayokki.mentor@gmail.com>
2024-09-18 02:47:09 +0900

    redis 本番用設定

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-REDIS = Redis.new(url: 'redis://localhost:6379/0')
+if Rails.env.production?
+  $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+else
+  REDIS = Redis.new(url: 'redis://localhost:6379/0')
+end
