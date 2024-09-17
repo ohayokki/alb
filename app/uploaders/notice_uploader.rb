2d619df0bedbbbde15b3676e79d57db49e6e9b38
obara811 <ohayokki.mentor@gmail.com>
2024-09-17 16:00:54 +0900

    アイコン整列作業

--- app/uploaders/notice_uploader.rb
+++ app/uploaders/notice_uploader.rb
@@ -1,7 +1,5 @@
 class NoticeUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::RMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -12,6 +10,7 @@ class NoticeUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  process resize_to_fill: [450, 800, gravity = ::Magick::CenterGravity]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
