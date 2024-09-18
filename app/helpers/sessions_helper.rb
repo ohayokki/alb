bcf9050415c911a57efe47512510f5c619da4dc9
obara811 <ohayokki.mentor@gmail.com>
2024-09-18 19:41:55 +0900

    ヘッダーにログアウト追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,12 @@ module SessionsHelper
   def shop_logged_in?
     !!shop_obj
   end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def user_signed_in?
+    !!current_user
+  end
 end
