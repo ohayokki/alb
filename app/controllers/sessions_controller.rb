e9f7042b332cfd6f469c3de93373bca65fc3bcc6
obara811 <ohayokki.mentor@gmail.com>
2024-09-18 20:04:41 +0900

    LINEから情報取得

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,9 @@
 # app/controllers/sessions_controller.rb
 class SessionsController < ApplicationController
 
+  require 'net/http'
   require 'json'
-  #require 'typhoeus'
+  require 'uri'
   require 'securerandom'
 
   def login
@@ -24,24 +25,51 @@ class SessionsController < ApplicationController
 
   def create
     # CSRF対策のトークンが一致する場合のみ、ログイン処理を続ける
-    if params[:state] == session[:state]
+    # ステートが一致しない場合はエラー処理
+    state = params[:state]
+    return redirect_to root_path, alert: '不正なアクセスです' if state != session[:state]
+      
 
-      puts "@@@@@@@@@@@@@"
 
-      puts params.inspect
-      line_user_id = ENV['LINE_REDIRECT_URI']
-      user = User.find_or_initialize_by(line_uid: line_user_id)
+    code = params[:code]
 
-      if user.save
-        session[:user_id] = user.id
-        redirect_to root_path, notice: 'ログインしました'
-      else
-        redirect_to login_path, notice: 'ログインに失敗しました'
-      end
+    puts "@@@@@@@@@@@@@"
 
-    else
-      redirect_to login_path, notice: '不正なアクセスです'
-    end
+    puts params.inspect
+    line_user_id = ENV['LINE_REDIRECT_URI']
+    user = User.find_or_initialize_by(line_uid: line_user_id)
+
+    code = params[:code]
+   
+    uri = URI('https://api.line.me/oauth2/v2.1/token')
+    res = Net::HTTP.post_form(uri, {
+      'grant_type' => 'authorization_code',
+      'code' => code,
+      'redirect_uri' => ENV['LINE_REDIRECT_URI'],
+      'client_id' => ENV['LINE_CHANNEL_ID'],
+      'client_secret' => ENV['LINE_CHANNEL_SECRET']
+    })
+  
+    token_info = JSON.parse(res.body)
+    access_token = token_info['access_token']
+  
+    uri = URI('https://api.line.me/v2/profile')
+    req = Net::HTTP::Get.new(uri)
+    req['Authorization'] = "Bearer #{access_token}"
+  
+    http = Net::HTTP.new(uri.host, uri.port, use_ssl: true)
+    res = http.request(req)
+  
+    user_info = JSON.parse(res.body)
+  
+    @user = User.find_or_initialize_by(line_uid: user_info['userId'])
+    @user.name = user_info['displayName']
+    @user.image_url = user_info['pictureUrl']
+    @user.save
+  
+    session[:user_id] = @user.id
+  
+    redirect_to root_path, notice: 'Logged in successfully!'
   end
 
 
@@ -51,32 +79,5 @@ class SessionsController < ApplicationController
   end
 
   private
-  def get_line_user_id_token(code)
-
-    # ユーザーのアクセストークン（IDトークン）を取得する
-    # https://developers.line.biz/ja/reference/line-login/#issue-access-token
-
-    url = 'https://api.line.me/oauth2/v2.1/token'
-
-    options = {
-      headers: {
-        'Content-Type' => 'application/x-www-form-urlencoded'
-      },
-      body: {
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: redirect_uri,
-        client_id: 'LINEログインチャネルのチャネルID', # 本番環境では環境変数などに保管
-        client_secret: 'LINEログインチャネルのチャネルシークレット' # 本番環境では環境変数などに保管
-      }
-    }
-    response = Typhoeus::Request.post(url, options)
-
-    if response.code == 200
-      JSON.parse(response.body)['id_token'] # ユーザー情報を含むJSONウェブトークン（JWT）
-    else
-      nil
-    end
-  end
 
 end
