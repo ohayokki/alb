dd23d83f7b0c1b3eecfd5f7a2952533b15482cb7
obara811 <ohayokki.mentor@gmail.com>
2024-09-18 17:27:40 +0900

    リダイレクト確認てすと

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+
+  config.action_controller.allow_forgery_protection = true
+  config.hosts << "access.line.me"
 end
